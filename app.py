9b5ea5d5b8f0b217b1634bc370a9b00c5df9bc16
Revy1664 <steinsgate95@mail.ru>
2024-09-19 11:50:13 +0600

    some changes of code

--- app.py
+++ app.py
@@ -5,59 +5,69 @@ from config import app, db
 from models import Card
 
 
-# routes
+# ROUTES
 @app.route("/")
 def index():
+    # get all the cards from database
+    cards = Card.query.order_by(Card.id.asc()).all()
 
-	cards = Card.query.all()
+    return render_template("index.html", cards=cards)
 
-	return render_template("index.html", cards=cards)
 
 @app.route("/create_card", methods=["POST", "GET"])
 def create_card():
 
-	if request.method == "POST":
-		# get data from form
-		word = request.form.get("word")
-		context = request.form.get("context")
+    if request.method == "POST":
+        # getting data from form
+        word = request.form.get("word")
+        context = request.form.get("context")
 
-		# add user to database
-		new = Card(word=word, context=context)
-		db.session.add(new)
-		db.session.commit()
+        # adding user to database
+        new = Card(word=word, context=context)
+        db.session.add(new)
+        db.session.commit()
 
-		return redirect("/", code=302)
-	
-	return render_template("create_card.html")
+        # redirecting to index
+        return redirect("/", code=302)
+
+    return render_template("create_card.html")
+
 
 @app.route("/remove/<int:card_id>")
 def delete_card(card_id: int):
+    # getting card by id
+    card = Card.query.get(card_id)
 
-	card = Card.query.get(card_id)
+    # deleting card from database and applying
+    db.session.delete(card)
+    db.session.commit()
 
-	db.session.delete(card)
-	db.session.commit()
+    # redirecting to index
+    return redirect("/", code=302)
 
-	return redirect("/", code=302)
 
 @app.route("/edit_card/<int:card_id>", methods=["POST", "GET"])
 def edit_card(card_id: int):
+    # getting card by id
+    card = Card.query.get(card_id)
 
-	if request.method == "POST":
+    if request.method == "POST":
+        # getting data from form
+        word = request.form.get("word")
+        context = request.form.get("context")
 
-		edited_word = request.form.get("word")
-		edited_context = request.form.get("context")
+        # writing edited data
+        card.word = word
+        card.context = context
 
-		card = Card.query.get(card_id)
+        # applying changes
+        db.session.commit()
 
-		card.word = edited_word
-		card.context = edited_context
+        # redirecting to index
+        return redirect("/", code=302)
 
-		db.session.commit()
+    return render_template("edit_card.html", card=card)
 
-		return redirect("/", code=302)
-
-	return render_template("edit_card.html")
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -2,9 +2,9 @@ from config import db, app
 
 
 class Card(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	word = db.Column(db.String(50), nullable=False)
-	context = db.Column(db.Text, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(50), nullable=False)
+    context = db.Column(db.Text, nullable=False)
 
-	def __str__(self):
-		return self.word
+    def __str__(self):
+        return self.word
