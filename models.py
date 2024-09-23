dc03ade4dfcbf854bb23ec7a712308a17403aa8a
Revy1664 <steinsgate95@mail.ru>
2024-09-23 21:34:02 +0600

    added user model, made it so that the password was saved as a hash (p.s. too many migrations because of i was trying to achive saving password as a hash)

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from config import db, app
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import bcrypt
 
 
 class Card(db.Model):
@@ -8,3 +10,25 @@ class Card(db.Model):
 
     def __str__(self):
         return self.word
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(30), unique=True)
+    _password_hash = db.Column("password_hash", db.String(256), nullable=False)
+    
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password_hash = password
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash  # Возвращаем значение хеша
+
+    @password_hash.setter
+    def password_hash(self, new_pass):
+        self._password_hash = bcrypt.generate_password_hash(new_pass).decode("utf-8")
