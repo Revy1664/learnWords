1b6de4458b500a20bf9ae323a0b17113237399bd
Revy1664 <steinsgate95@mail.ru>
2024-10-03 19:50:49 +0600

    add author to card model, now when a user creates a card, he becomes its author automatically

--- models.py
+++ models.py
@@ -1,16 +1,9 @@
-from flask_login import UserMixin
+from flask_login import UserMixin, current_user
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy import event
 
 from config import db, app, bcrypt, login_manager
-
-
-class Card(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(50), nullable=False)
-    context = db.Column(db.Text, nullable=False)
-
-    def __str__(self):
-        return self.word
 
 
 class User(UserMixin, db.Model):
@@ -20,6 +13,11 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(30), unique=True)
     _password_hash = db.Column("password_hash", db.String(256), nullable=False)
+    cards: Mapped[list["Card"]] = relationship(
+        "Card",
+        back_populates="user",
+        cascade="all, delete, save-update",
+    )
     
     def __init__(self, username, email, password):
         self.username = username
@@ -38,3 +36,23 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(50), nullable=False)
+    context = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user: Mapped[User] = relationship(
+        "User",
+        back_populates="cards"
+    )
+
+    def __str__(self):
+        return self.word
+
+
+@event.listens_for(Card, "before_insert")
+def set_user_id(mapper, connection, target):
+    if not target.user_id:
+        target.user_id = current_user.id
